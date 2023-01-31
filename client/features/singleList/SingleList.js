9b445b298a59c8d38dba5c1cee311426a0446dc8
Downtown2-6 <t.fares85@gmail.com>
2023-01-31 17:55:36 -0500

    tasks are draggable, but do not persist yet.

--- client/features/singleList/SingleList.js
+++ client/features/singleList/SingleList.js
@@ -2,6 +2,10 @@ import React, { useState }from "react";
 import { useDispatch } from "react-redux";
 import { addTaskCard } from "../taskCards/taskCardsSlice";
 import SingleTaskCard from "../taskCards/SingleTaskCard";
+import { Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const ListContainer = styled.div``
 
 const SingleList = (props) => {
   const [taskCardName, setTaskCardName] = useState('');
@@ -16,42 +20,54 @@ const SingleList = (props) => {
   const handleSubmitTaskCard = async (evt) => {
     evt.preventDefault();
     if (taskCardName.length) {
-      await dispatch(addTaskCard({ 
-        boardId, 
-        listId, 
-        taskcardName: taskCardName, 
-        position: numTaskCards 
+      await dispatch(addTaskCard({
+        boardId,
+        listId,
+        taskcardName: taskCardName,
+        position: numTaskCards
       }));
       setTaskCardName('');
     }
   }
 
   return (
-    <div>
-      <h4>{list.listName}</h4>
-      <div className='list-taskCards-container'>
-        {list.taskcards && list.taskcards.length ? list.taskcards.map((taskCard) => (
-          <div key={`taskCard#${taskCard.id}`} className='taskCard'>
-            <SingleTaskCard taskCard={taskCard} />
-          </div>
-        )) : null}
+    <Droppable droppableId={listId.toString()}>
+      {(provided, snapshot) => (
+        <ListContainer
+          innerRef={provided.innerRef}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          isDragging={snapshot.isDragging}
+        >
+          <div>
+            <h4>{list.listName}</h4>
+            <div className='list-taskCards-container'>
+              {list.taskcards && list.taskcards.length ? list.taskcards.map((taskCard, index) => (
+                <div key={`taskCard#${taskCard.id}`} className='taskCard'>
+                  <SingleTaskCard taskCard={taskCard} index={index} />
+                </div>
+              )) : null}
 
-      </div>
-      <div className='list-bottom-container'>
-        <form className='add-taskCard-form' onSubmit={handleSubmitTaskCard}>
-          <input 
-            className='add-taskCard' 
-            name='taskcardName'
-            type='text'
-            value={taskCardName}
-            onChange={(evt) => setTaskCardName(evt.target.value)}
-          />
-          <button className='add-taskCard-button' type='submit'>
-            Add card
-          </button>
-        </form>
-      </div>
-    </div>
+            </div>
+            <div className='list-bottom-container'>
+              <form className='add-taskCard-form' onSubmit={handleSubmitTaskCard}>
+                <input
+                  className='add-taskCard'
+                  name='taskcardName'
+                  type='text'
+                  value={taskCardName}
+                  onChange={(evt) => setTaskCardName(evt.target.value)}
+                />
+                <button className='add-taskCard-button' type='submit'>
+                  Add card
+                </button>
+              </form>
+            </div>
+          </div>
+        </ListContainer>
+      )}
+    </Droppable>
   )
 }
 
--- client/features/taskCards/SingleTaskCard.js
+++ client/features/taskCards/SingleTaskCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Draggable } from 'react-beautiful-dnd'
+import styled from 'styled-components';
+
+const TaskContainer = styled.div ``
+
 
 const SingleTaskCard = (props) => {
   const taskCard = props.taskCard;
-
+console.log(taskCard)
   return (
-    // <Link>
-      <p>{taskCard.taskcardName}</p>
-    // </Link>
+    <Draggable draggableId={taskCard.id.toString()} index={props.index}>
+      {(provided, snapshot) => (
+      <TaskContainer
+        innerRef={provided.innerRef}
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        isDragging={snapshot.isDragging}
+      >
+        <p>{taskCard.title}</p>
+      </TaskContainer>
+      )}
+    </Draggable>
   )
 }
 
