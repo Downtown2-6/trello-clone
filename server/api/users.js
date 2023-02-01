452e6771662cfb473acfa4b6140293e2f2ac72b8
EightFiveZeroEight <Business.8508@gmail.com>
2023-02-01 12:56:33 -0500

    Refactor api/users.js to be easier to read

--- server/api/users.js
+++ server/api/users.js
@@ -21,13 +21,9 @@ router.get("/", async (req, res, next) => {
 // GET /api/users/allBoards/:userId
 router.get("/allBoards/:userId", async (req, res, next) => {
   try {
-    console.log(
-      "This\n--is\n--inside\n--of\n--the\n--api\n--route\n--for\n--boards/user",
-      req.params.userId
-    );
-    const theUserId = req.params.userId;
+    const {userId} = req.params;
     const boards = await UserBoard.findAll({
-      where: { userId: theUserId },
+      where: { userId},
       include: { model: Board },
     });
     res.json(boards);
@@ -43,10 +39,9 @@ router.get("/specificBoard/:userId/:boardId", async (req, res, next) => {
       "This\n--is\n--inside\n--of\n--the\n--api\n--route\n--for\n--boards/user",
       req.params.userId
     );
-    const theUserId = req.params.userId;
-    const boardId = req.params.boardId;
+    const {userId, boardId} = req.params;
     const boards = await UserBoard.findAll({
-      where: { userId: theUserId, boardId: boardId },
+      where: { userId, boardId},
     });
     res.json(boards);
   } catch (error) {
@@ -57,8 +52,7 @@ router.get("/specificBoard/:userId/:boardId", async (req, res, next) => {
 // PUT /api/users/grantAccess/:boardId/:userId
 router.put("/grantAccess/:boardId/:userId", async (req, res, next) => {
   try {
-    const boardId = req.params.boardId;
-    const userId = req.params.userId;
+    const {boardId, userId} = req.params;
     const privilege = "USER";
     const boards = await UserBoard.findAll({
       where: { userId: userId, boardId: boardId },
@@ -80,21 +74,28 @@ router.put("/grantAccess/:boardId/:userId", async (req, res, next) => {
   }
 });
 
-// PUT /api/users/modifyPrivilege/:boardId/:userId
-router.put("/modifyPrivilege/:boardId/:userId", async (req, res, next) => {
-  try {
-    const boardId = req.params.boardId;
-    const userId = req.params.userId;
-    const { privilege } = req.body;
-    const userBoard = await UserBoard.findOne({ where: { boardId, userId } });
-    if (!userBoard) {
-      return res.status(404).json({ message: "User not found in the board" });
+// PUT /api/users/modifyPrivilege/:userIssuingRequestId/:boardId/:userId
+router.put(
+  "/modifyPrivilege/:userIssuingRequestId/:boardId/:userId",
+  async (req, res, next) => {
+    try {
+      const { userIssuingRequestId: issuer, boardId, userId } = req.params;
+      const theIssuer = await UserBoard.findOne({
+        where: { boardId, userId:issuer },
+      });
+      console.log("Is the issuer an ADMIN?", theIssuer.privilege==="ADMIN");
+
+      const { privilege } = req.body;
+      const userBoard = await UserBoard.findOne({ where: { boardId, userId } });
+      if (!userBoard) {
+        return res.status(404).json({ message: "User not found in the board" });
+      }
+      await userBoard.update({ privilege });
+      res.status(200).json({ message: "User privilege updated successfully" });
+    } catch (err) {
+      next(err);
     }
-    await userBoard.update({ privilege });
-    res.status(200).json({ message: "User privilege updated successfully" });
-  } catch (err) {
-    next(err);
   }
-});
+);
 
 module.exports = router;
