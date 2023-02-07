029f2b9e7ef40ead8cc369b7da0ba8b6c5b732ee
EightFiveZeroEight <Business.8508@gmail.com>
2023-02-07 15:34:39 -0500

    Create front-end user update component

---
+++ client/features/userProfile/UserProfile.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function UserProfile() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event)=>{
+      console.log(`***
+      ***
+      ***
+      Logging:This is the event in UserProfile
+      ***
+      ***
+      ***
+      `, event);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(event) => setFirstName(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(event) => setLastName(event.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit">Update User</button>
+    </form>
+  );
+}
+
+export default UserProfile;
