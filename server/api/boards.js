e6fc04b88b8e37482331def05f08e52082924e39
EightFiveZeroEight <Business.8508@gmail.com>
2023-02-09 15:52:24 -0500

    Fix boards.js delete route

--- server/api/boards.js
+++ server/api/boards.js
@@ -17,6 +17,36 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+// PATCH /api/boards/archiveBoard/boardId/:boardId/userRequesting/:userId
+router.patch(
+  "/archiveBoard/boardId/:boardId/userRequesting/:userId",
+  async (req, res, next) => {
+    try {
+      const { boardId, userId } = req.params;
+      const getUser = await UserBoard.findAll({
+        where: { userId: userId, boardId: boardId },
+      });
+      const userPrivilege = getUser[0].dataValues.privilege;;
+
+      if (userPrivilege != "ADMIN")
+        return res.status(401).json("User must be an admin to do this.");
+      const board = await Board.findByPk(boardId);
+
+      const archiveBoard = await Board.update(
+        { isArchived: !board.isArchived },
+        { where: { id: boardId } }
+      );
+
+      const updatedBoard = await Board.findByPk(boardId);
+
+      res.status(201).json(updatedBoard)
+
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
 // GET /api/boards/allUsers/:boardId
 router.get("/allUsers/:boardId", async (req, res, next) => {
   try {
@@ -36,15 +66,27 @@ router.delete(
   "/thisUser/:userId/thisBoard/:boardId",
   async (req, res, next) => {
     try {
+      console.log(`***
+      ***
+      ***
+      Logging:userId, boardId
+      ***
+      ***
+      ***
+      `, req.params.userId, req.params.boardId);
+      const theUserBoardAssociation = await UserBoard.findOne({
+        where: { userId: req.params.userId, boardId:req.params.boardId },
+      });
+      if (!theUserBoardAssociation)
+        res.status(404).json("User Not Found");
+
+      const theUserRemoved = theUserBoardAssociation.dataValues;
       const deleteUserBoard = await UserBoard.destroy({
         where: { userId: req.params.userId, boardId: req.params.boardId },
       });
-      res.status(201).json({
-        associationRemoved: deleteUserBoard,
-        message: "The association was removed. ",
-      });
-    } catch (error) {
-      next(error);
+      res.status(201).json(theUserRemoved);
+    } catch (err) {
+      next(err);
     }
   }
 );
